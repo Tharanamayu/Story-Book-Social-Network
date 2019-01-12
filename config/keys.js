ad6442edf0d784b5e2c11cb086232046e530df77
tharanamayu <tharana.maya@gmail.com>
2019-01-12 18:21:07 +0530

    connecting with google auth api

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+
+mongoURI:'mongodb://tharanamayu:anshibaba2120@ds155614.mlab.com:55614/storybook-dev',
+googleClientID:'191080598332-n4fc323qjcmo77389i054vbgrvj9uqnf.apps.googleusercontent.com',
+googleClientSecret:'a-JWcML6xvxgfFdg_06YxuNl'
+}
